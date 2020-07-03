bac65a1d523bbb1dd1822fc03264b79a50ebf67a
gagan <gaganvamsi2@gmail.com>
2020-07-03 08:02:05 +0530

    initial commit

---
+++ build/precache-manifest.b7a76e1dfd49081c22158926e4fb8f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd086e8ff95a25be9905c606c5f3458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f249962d7e6895f5f639",
+    "url": "/static/css/2.5ffa1744.chunk.css"
+  },
+  {
+    "revision": "f249962d7e6895f5f639",
+    "url": "/static/js/2.fd348c4d.chunk.js"
+  },
+  {
+    "revision": "a51e1a732616a65fe21698cc04856fb1",
+    "url": "/static/js/2.fd348c4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "524e5df53db6a57544c9",
+    "url": "/static/js/main.ea5f33fb.chunk.js"
+  },
+  {
+    "revision": "1aacdb1eb65d59c83b59",
+    "url": "/static/js/runtime-main.871a3edc.js"
+  }
+]);
