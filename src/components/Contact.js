ce591949a9d1eb3478d24d805b4655ec4e675c74
gagan <gaganvamsi2@gmail.com>
2020-12-07 22:00:30 +0530

    google form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ class Contact extends React.Component {
         <div className="container">
           <div className="row">
             <div className="col-lg-8 offset-lg-2">
-              <h1 className="section-title text-center">Get In Touch</h1>
+              <h1 className="section-title text-center">CONTACT US</h1>
               <div className="section-title-border margin-t-20"></div>
               <p className="section-subtitle text-muted text-center font-secondary padding-t-30">
                 We thrive when coming up with innovative ideas but also
@@ -44,75 +44,10 @@ class Contact extends React.Component {
             </div>
             <div className="col-lg-8">
               <div className="custom-form mt-4 pt-4">
-                <div id="message"></div>
-                <form
-                  method="post"
-                  action="home"
-                  name="contact-form"
-                  id="contact-form"
-                >
-                  <div className="row">
-                    <div className="col-lg-6">
-                      <div className="form-group mt-2">
-                        <input
-                          name="name"
-                          id="name"
-                          type="text"
-                          className="form-control"
-                          placeholder="Your name*"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-lg-6">
-                      <div className="form-group mt-2">
-                        <input
-                          name="email"
-                          id="email"
-                          type="email"
-                          className="form-control"
-                          placeholder="Your email*"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-lg-12">
-                      <div className="form-group mt-2">
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="subject"
-                          placeholder="Your Subject.."
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-lg-12">
-                      <div className="form-group mt-2">
-                        <textarea
-                          name="comments"
-                          id="comments"
-                          rows="4"
-                          className="form-control"
-                          placeholder="Your message..."
-                        ></textarea>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-lg-12 text-right">
-                      <input
-                        type="submit"
-                        id="submit"
-                        name="send"
-                        className="submitBnt btn btn-custom"
-                        value="Send Message"
-                      />
-                      <div id="simple-msg"></div>
-                    </div>
-                  </div>
-                </form>
+                <div id="message">
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfL0M4q_1MfIAyakT33XvVNpfKRjRe3B9zzcIp5wCbOOQNbrg/viewform?embedded=true" width="640" height="300" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                </div>
+
               </div>
             </div>
           </div>
